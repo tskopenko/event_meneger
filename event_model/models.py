c8da8d00885842ee093fdd0884d93ce6cdb9fa22
Tymofii Skopenko <youmustdeth@gmail.com>
2025-07-15 13:35:44 +0300

    init + event model + gitignore up

---
+++ event_model/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from user.models import User
+from django.db.models.functions import Now
+from location_field.models.plain import PlainLocationField
+
+class Event(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255)
+    date = models.DateField(db_default=Now())
+    city = models.CharField(max_length=255)
+    location = PlainLocationField(based_fields=['city'])
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
+
+    class Meta:
+        verbose_name_plural = 'events'
+
+    def __str__(self):
+        return f'Event: {self.title} at {self.date} at {self.location} was created!'
+
